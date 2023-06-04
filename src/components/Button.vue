e83bf070504cf90d608504b94b3c242aaa68372c
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-06-04 13:17:19 +0200

    added button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  background-color: var(--grey);
+  color: var(--light);
+  border: 1px solid var(--grey);
+  padding: 1em;
+  border-radius: 0.5em;
+
+  &:hover {
+    background-color: var(--primary-alt);
+  }
+}
+</style>
